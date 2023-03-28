d6de26e43d2ea5ceeb9a23561cf5d93f1e64f94b
Patricio <gablake8@hotmail.com>
2023-03-28 00:05:42 -0500

    second commit

---
+++ src/main/java/com/example/Patrickcomplaint/PatrickComplaintApplication.java
@@ -0,0 +1,13 @@
+package com.example.Patrickcomplaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrickComplaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrickComplaintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Patrickcomplaint/PatrickComplaintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Patrickcomplaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrickComplaintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
